0f88668a3a71c74eaeb939bea9f2abaef1197d4d
IronMan <zym2894209453@163.com>
2024-06-06 14:29:48 +0800

    test

--- test01/src/main/java/com/zym/App.java
+++ test01/src/main/java/com/zym/App.java
@@ -1,13 +1,29 @@
 package com.zym;
 
+import HslCommunication.Core.Address.ModbusAddress;
+import HslCommunication.Core.Types.OperateResult;
+import HslCommunication.ModBus.ModbusTcpNet;
+import HslCommunication.Profinet.Siemens.SiemensPLCS;
+import HslCommunication.Profinet.Siemens.SiemensS7Net;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    // 创建Modbus TCP客户端对象
+    private static ModbusTcpNet modbusClient;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        modbusClient = new ModbusTcpNet();
+        modbusClient.setIpAddress("192.168.0.1");
+        modbusClient.setPort(502);
+        OperateResult operateResult = modbusClient.ConnectServer();
+//        modbusClient.Read()
+
+        System.out.println(operateResult.Message);
+
     }
 }
+
--- test01/src/main/java/com/zym/utils/PlcUtils.java
+++ test01/src/main/java/com/zym/utils/PlcUtils.java
@@ -1,2 +1,4 @@
-package com.zym.utils;public class PlcUtils {
+package com.zym.utils;
+
+public class PlcUtils {
 }
