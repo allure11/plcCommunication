fff88df712919edc687caf62dc92b925e6c6bc85
IronMan <zym2894290453@163.com>
2024-06-06 20:48:07 +0800

    测试读取数据

--- test01/src/main/java/com/zym/App.java
+++ test01/src/main/java/com/zym/App.java
@@ -1,28 +1,33 @@
 package com.zym;
 
-import HslCommunication.Core.Address.ModbusAddress;
 import HslCommunication.Core.Types.OperateResult;
+import HslCommunication.Core.Types.OperateResultExOne;
 import HslCommunication.ModBus.ModbusTcpNet;
-import HslCommunication.Profinet.Siemens.SiemensPLCS;
-import HslCommunication.Profinet.Siemens.SiemensS7Net;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    // 创建Modbus TCP客户端对象
+public class App {
+    /**
+     * 创建Modbus TCP客户端对象
+     */
     private static ModbusTcpNet modbusClient;
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         modbusClient = new ModbusTcpNet();
-        modbusClient.setIpAddress("192.168.0.1");
+        modbusClient.setIpAddress("192.168.21.128");
         modbusClient.setPort(502);
         OperateResult operateResult = modbusClient.ConnectServer();
-//        modbusClient.Read()
+        if (operateResult.IsSuccess == true) {
+//            short[] number = new short[189];
+//            modbusClient.Write("400000", number);
+            OperateResultExOne<byte[]> read = modbusClient.Read("400000", (short) 10);
+            // 共计10个字节的结果内容
+            int alarm = modbusClient.getByteTransform().TransInt16(read.Content, 4);
 
-        System.out.println(operateResult.Message);
+
+            System.out.println(alarm);
+        }
 
     }
 }
