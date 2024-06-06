b67b4b6ebe17d7dfefe17dc55bf0796b2c542492
IronMan <zym2894290453@163.com>
2024-06-07 00:09:48 +0800

    测试读取数据

--- test01/src/main/java/com/zym/App.java
+++ test01/src/main/java/com/zym/App.java
@@ -21,12 +21,26 @@ public class App {
         if (operateResult.IsSuccess == true) {
 //            short[] number = new short[189];
 //            modbusClient.Write("400000", number);
-            OperateResultExOne<byte[]> read = modbusClient.Read("400000", (short) 10);
+            OperateResultExOne<short[]> read = modbusClient.ReadInt16("0", (short) 10);
+            OperateResultExOne<short[]> read3 = modbusClient.ReadInt16("x=4;0", (short) 10);
+            OperateResultExOne<boolean[]> read2 = modbusClient.ReadBool("0", (short) 10);
+            OperateResultExOne<boolean[]> read4 = modbusClient.ReadBool("x=2;8", (short) 10);
             // 共计10个字节的结果内容
-            int alarm = modbusClient.getByteTransform().TransInt16(read.Content, 4);
+            for (short b : read.Content) {
+                System.out.println(b);
+            }
+            for (short b : read3.Content) {
+                System.out.println(b);
+            }
+            for (boolean b : read2.Content) {
+                System.out.println(b);
+            }
+            for (boolean b : read4.Content) {
+                System.out.println(b);
+            }
+//            int alarm = modbusClient.getByteTransform().TransInt16(read.Content, 1);
+//            int alarm2 = modbusClient.getByteTransform().TransInt16(read.Content, 1);
 
-
-            System.out.println(alarm);
         }
 
     }
